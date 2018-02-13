a734ab7604fffd0801c9f6152ccf96e3ad0119ba
dch <chuanhao.dai@drcuiyutao.com>
2018-02-13 14:31:01 +0800

    递归函数recursion

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,42 @@
 from square import power
 from recursion import recursion
 from variableparameter import calc, product
+from util import trim
 
+# 测试:
+if trim('hello  ') != 'hello':
+    print('1测试失败!', trim('hello  '))
+elif trim('  hello') != 'hello':
+    print('2测试失败!', trim('  hello'))
+elif trim('  hello  ') != 'hello':
+    print('3测试失败!', trim('  hello  '))
+elif trim('  hello  world  ') != 'hello  world':
+    print('4测试失败!', trim('  hello  world  '))
+elif trim('') != '':
+    print('5测试失败!', trim(''))
+elif trim('    ') != '':
+    print('6测试失败!', trim('    '))
+else:
+    print('测试成功!')
+
+print('******************')
+
+L = []
+n = 1
+i = 0
+while n <= 99:
+    L.insert(i, n)
+    i += 1
+    n += 2
+print(L[-2:])
+print([L[0], L[1], L[2]])  # 取出前三个元素笨方法
+r = []
+j = 3
+for x in range(j):
+    r.append(L[x])  # for取出前三个元素方法
+print(r)
+
+print('*******************')
 print(recursion(5))
 print(recursion(100))
 
---
+++ util.py
@@ -0,0 +1,11 @@
+def trim(s):
+    if '' == s:
+        print('1', s)
+        return ''
+    if s[:1] != ' ' and s[-1:] != ' ':
+        print('2', s)
+        return s
+    elif s[:1] == ' ':
+        return trim(s[1:])
+    else:
+        return trim(s[0:len(s) - 2])
