58d3ec1e77ac29233dd18e03e11514e1bc600585
dch <chuanhao.dai@drcuiyutao.com>
2018-03-28 15:08:13 +0800

    Email之smtp version3 开启加密ssl传输

--- com/drcuiyutao/py/email/smtp.py
+++ com/drcuiyutao/py/email/smtp.py
@@ -17,14 +17,16 @@ password = input('Password: ')
 to_addr = input('To: ')
 # 输入SMTP服务器地址:
 smtp_server = input('SMTP server: ')
+smtp_port = input('SMTP port: ')
 
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg = MIMEText('11hello, send by Python...', 'plain', 'utf-8')
 msg['From'] = _format_addr('Python学习者 <%s>' % from_addr)
 msg['To'] = _format_addr('管理员 <%s>' % to_addr)
-msg['Subject'] = Header('来自SMTP的问候......', 'utf-8').encode()
+msg['Subject'] = Header('11来自SMTP的问候......', 'utf-8').encode()
 
-server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
+server = smtplib.SMTP(smtp_server, smtp_port) # SMTP协议默认端口是25
+server.starttls()
 server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
