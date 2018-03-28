46948d090ff2d44c928219a402236463bcf13a2b
dch <chuanhao.dai@drcuiyutao.com>
2018-03-28 14:56:18 +0800

    Email之smtp version2

--- com/drcuiyutao/py/email/smtp.py
+++ com/drcuiyutao/py/email/smtp.py
@@ -1,6 +1,13 @@
+from email import encoders
+from email.header import Header
 from email.mime.text import MIMEText
+from email.utils import parseaddr,formataddr
+import smtplib
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+
+def _format_addr(s):
+    name,addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
 
 
 # 输入Email地址和命令:
@@ -11,9 +18,14 @@ to_addr = input('To: ')
 # 输入SMTP服务器地址:
 smtp_server = input('SMTP server: ')
 
-import smtplib
+
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg['From'] = _format_addr('Python学习者 <%s>' % from_addr)
+msg['To'] = _format_addr('管理员 <%s>' % to_addr)
+msg['Subject'] = Header('来自SMTP的问候......', 'utf-8').encode()
+
 server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
-# server.set_debuglevel(1)
+server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
 server.quit()
